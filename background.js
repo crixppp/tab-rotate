03530e0ac5ed214fa69090a8d346f3e2b9bb879c
crixppp <samgilliot1@gmail.com>
2025-04-27 23:37:24 +1000

    Bug fixes

--- background.js
+++ background.js
@@ -5,8 +5,18 @@ chrome.action.onClicked.addListener((tab) => {
   chrome.scripting.executeScript({
     target: { tabId: tab.id },
     func: (rotation) => {
-      document.body.style.transform = `rotate(${rotation}deg)`;
-      document.body.style.transformOrigin = "center center";
+      const body = document.body;
+      body.style.transformOrigin = "center center";
+
+      if (rotation === 90) {
+        body.style.transform = "rotate(90deg) scale(" + window.innerHeight / window.innerWidth + ")";
+      } else if (rotation === 180) {
+        body.style.transform = "rotate(180deg) scale(1)";
+      } else if (rotation === 270) {
+        body.style.transform = "rotate(270deg) scale(" + window.innerHeight / window.innerWidth + ")";
+      } else {
+        body.style.transform = "rotate(0deg) scale(1)";
+      }
     },
     args: [rotation]
   });
